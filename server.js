29fa2b671503260c25248bd527cd70ae36f06d9e
Kermit Joseph Lee <kermitjosephlee@gmail.com>
2018-08-24 12:40:12 -0400

    updated server file

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const app = express();
-const cookieSession = require("cookie-session");
 const bodyParser = require("body-parser");
-const bcrypt = require("bcryptjs");
-const saltRounds = 10;
 const PORT = 8080;
+
+//****************************************
+
+const ticketParser = riderObj => {
+  const tempObj = JSON.parse(riderObj);
+  return tempObj;
+};
+
+//****************************************
+
+app.get("/"),
+  (req, res) => {
+    res.render("/");
+  };
+
+app.post("/help_request"), (req, res) => {};
+
+//****************************************
+
+app.listen(PORT, () => {
+  console.log("PitCrew app listening on port ${PORT}!");
+  console.log("ooo eee can do!");
+});
