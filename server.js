d9e3167e6e50f404497fb808403016169b2892e5
Kermit Joseph Lee <kermitjosephlee@gmail.com>
2018-08-26 12:09:17 -0400

    took out old routes -- starting again with a new model - got server file to run on nodemon

--- server.js
+++ server.js
@@ -33,53 +33,17 @@ const ticketParser = riderObj => {
 
 //****************************************
 
-// -1. DISPATCH Page
-app.get("/dispatch", (req, res) => {
-  res.render("/dispatch");
-});
-
-// 0. Welcome page render
+// 1. Generic USER gets login page
 app.get("/", (req, res) => {
-  res.render("/login");
+  res.render("main");
 });
 
-// 1. RIDER calls for help
-// will need logic to evaulate riderObj to see if we get all the info we need to create a ticket
-app.post("/:rider/", (req, res) => {
-  let riderObj = req.body.riderObj;
-  let tempTicket = ticketParser(riderObj);
-  let ticketId = res.redirect("/:rider/HelpHasBeenCalled");
+// 2. RIDER posts login information
+app.post("/login", (req, res) => {
+  const userName = req.body.userName;
+  const password = req.body.password;
+  res.redirect("/:rider");
 });
-
-// 2. DISPATCH receives call for help and send a pending call render to rider
-app.get("/:rider/HelpHasBeenCalled", (req, res) => {
-  res.render("/:rider/HelpHasBeenCalled");
-});
-
-// 3. DISPATCH assigns ticket to potential TECHS
-// 4. TECHS receive pending ticket requests
-app.get("/newTickets/:tech", (req, res) => {
-  res.render("/newTickets");
-});
-
-// 5. One TECH confirms ticket - changing pending ticket status to ACTIVE - sends POST to DISPATCH
-app.post("/newTickets/:tech", (req, res) => {
-  let ticketStatus = req.body.acceptOrDecline;
-  if (ticketStatus) {
-    res.redirect("/riderLocation");
-  } else {
-    res.redirect("/index");
-  }
-});
-
-// 6. DISPATCH receives confirm from TECH - updates ticket status
-app.get("/");
-
-// 7. DISPATCH sends update to RIDER
-// 8. TECH arrives at RIDER - sends update to DISPATCH
-// 9. TECH finishes ticket - sends update to DISPATCH
-// 10. RIDER ticket update
-// 11. DISPATCH changes status & archives ticket
 
 //****************************************
 
