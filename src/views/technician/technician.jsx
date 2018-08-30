a910033d23f2c0afea39736cda6383b444aa0ba9
Michael Surya <tomichaels@gmail.com>
2018-08-30 18:27:11 +0000

    first tech commit

--- src/views/technician/technician.jsx
+++ src/views/technician/technician.jsx
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { GoogleMap } from "react-google-maps";
+import { compose, withProps } from "recompose";
+import {
+  withGoogleMap,
+  GoogleMap,
+  withScriptjs,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
 
 class Technician extends Component {
   render() {
@@ -12,4 +19,14 @@ class Technician extends Component {
   }
 }
 
-export default Technician;
+export default compose(
+  withProps({
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyCHs0Po1ZjrqqKy8pNXcXX3Gfl71w2GEDs&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: "100%" }} />,
+    containerElement: <div style={{ height: "500px", width: "500px" }} />,
+    mapElement: <div style={{ height: "100%" }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(Technician);
