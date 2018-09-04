5b0614fc8d7fbca2a7c6bc293e2e09f1ab89f7fe
Kermit Joseph Lee <kermitjosephlee@gmail.com>
2018-09-04 10:05:28 -0400

    took out unnecessary libraries

---
+++ pitcrew-native/rider_summary.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { StackNavigator } from "react-navigation";
+import {
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+  NavigatorIOS,
+  Button,
+  Container,
+  Picker
+} from "react-native";
+import { MapView } from "expo";
+import GetLocation from "./getLocation"
+import RiderMap from "./riderMap"
